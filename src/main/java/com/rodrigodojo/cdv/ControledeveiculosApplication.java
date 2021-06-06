4709820571fb12c6cf780d17e62a4e485ce69ba5
Rodrigo Dojo <rodrigodoj@gmail.com>
2021-06-06 12:52:10 -0300

    projeto criado

---
+++ src/main/java/com/rodrigodojo/cdv/ControledeveiculosApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigodojo.cdv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControledeveiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControledeveiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigodojo/cdv/ControledeveiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigodojo.cdv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControledeveiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
