43c1aa280706efcff45c16c41a807a5b7fc9b26b
Rodrigo Dojo <rodrigodoj@gmail.com>
2021-06-08 10:57:56 -0300

    adiciodado hashcodes

--- src/main/java/com/rodrigodojo/cdv/domain/Usuario.java
+++ src/main/java/com/rodrigodojo/cdv/domain/Usuario.java
@@ -62,5 +62,28 @@ public class Usuario {
 	public void setDataDeNascimento(Date dataDeNascimento) {
 		this.dataDeNascimento = dataDeNascimento;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+	
 	
 }
--- src/main/java/com/rodrigodojo/cdv/domain/Veiculo.java
+++ src/main/java/com/rodrigodojo/cdv/domain/Veiculo.java
@@ -50,5 +50,27 @@ public class Veiculo {
 	public void setAno(Integer ano) {
 		this.ano = ano;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Veiculo other = (Veiculo) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
 	
 }
