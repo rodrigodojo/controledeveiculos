9f520f6ff84222dbed733eb082487e472e556e7b
Rodrigo Dojo <rodrigodoj@gmail.com>
2021-06-11 20:11:36 -0300

    corrigindo code

--- src/main/java/com/rodrigodojo/cdv/resources/VeiculoResource.java
+++ src/main/java/com/rodrigodojo/cdv/resources/VeiculoResource.java
@@ -5,9 +5,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -43,6 +45,18 @@ public class VeiculoResource {
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Veiculo> update(@PathVariable Integer id,@RequestBody Veiculo obj){
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);	
 	}
 	
 }
--- src/main/java/com/rodrigodojo/cdv/services/VeiculoService.java
+++ src/main/java/com/rodrigodojo/cdv/services/VeiculoService.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.rodrigodojo.cdv.domain.Usuario;
 import com.rodrigodojo.cdv.domain.Veiculo;
 import com.rodrigodojo.cdv.repositories.VeiculoRepository;
 
